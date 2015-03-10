81fa24b44917a237d8523049b4dbea9e79d71a2b
孙渊汇 <syhkiller@163.com>
2015-03-10 15:01:49 +0800

    foce use boost 1.5.7

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -7,16 +7,26 @@ function exit_for {
 }
 
 dataSourceIP=syh-tj-ubuntu.local
+scriptDir=$(cd `dirname $0`;pwd)
+nprocessor=$(cat /proc/cpuinfo | grep processor | wc -l)
 which clang || sudo apt-get install -y clang || exit_for 'unable to install clang'
 which cmake || sudo apt-get install -y cmake || exit_for 'unable to install cmake'
-which showmount || sudo apt-get install -y nfs-common || exit_for 'unable to'
+which showmount || sudo apt-get install -y nfs-common || exit_for 'unable to install nfs client'
 find /usr/include/shapefil.h || sudo apt-get install -y libshp-dev || exit_for 'unbale to install libshp'
-[ -d /usr/local/include/boost ] || sudo apt-get install -y libboost1.55-all-dev || sudo apt-get install -y libboost-all-dev || exit_for 'unable to install boost'
+if [ -d /usr/local/include/boost ];
+then
+    boostTmp=$(mktemp -d /tmp/boost1.57.XXXXXX)
+    cd $boostTmp
+    wget http://jaist.dl.sourceforge.net/project/boost/boost/1.57.0/boost_1_57_0.tar.bz2 || exit_for 'unable to download boost 1.57'
+    tar -xvf boost_1_57_0.tar.bz2
+    cd boost_1_57_0
+    ./bootstrap.sh --prefix=/usr/local/
+    ./b2 -j$nprocessor
+    sudo ./b2 install
+fi
 
 git submodule update --init || exit_for 'unable to update submodule'
-
 #mkdirs
-scriptDir=$(cd `dirname $0`;pwd)
 workspaceDir=$scriptDir/workspace
 dataDir=$scriptDir/data
 GPSDir=$dataDir/GPS
@@ -28,7 +38,6 @@ trajDir=$dataDir/traj
 
 cd $buildDir
 CC=clang CXX=clang++ cmake $scriptDir -DCMAKE_BUILD_TYPE=Release
-nprocessor=$(cat /proc/cpuinfo | grep processor | wc -l)
 make install -j$nprocessor
 cd $workspaceDir
 
